84170c508d35fa07fc3f8cd403fd457f88ae3e7c
kamegu <kamegu@server.fake>
2013-05-22 22:32:24 +0900

    jQuery UI sortable placeholder

--- js/demo.js
+++ js/demo.js
@@ -15,7 +15,32 @@
 		$('head').append($('<script>').attr('src','/lib/syntaxhighlighter/scripts/shBrushCss.js'));
 	}
 })($);
+
+function buttonsOnOff(onElemId, offElemId, options) {
+	var $onElem = $('#' + onElemId);
+	var $offElem = $('#' + offElemId);
+	$onElem.button().click(function(){
+		if (options) {
+			if (options.onchange) options.onchange(true);
+			if (options.onchangeOn) options.onchangeOn();
+		}
+		$onElem.button( "option", "disabled", true );
+		$offElem.button( "option", "disabled", false );
+	});
+	$offElem.button().click(function(){
+		if (options) {
+			if (options.onchange) options.onchange(false);
+			if (options.onchangeOff) options.onchangeOff();
+		}
+		$onElem.button( "option", "disabled", false );
+		$offElem.button( "option", "disabled", true );
+	});
+	$onElem.trigger('click');
+}
+
 function initDemoPage($) {
+}
+function _initDemoPage($) {
 	if (!$) {
 		return;
 	}
@@ -23,13 +48,17 @@ function initDemoPage($) {
 		$block = $(this);
 		$js = $block.find('script');
 		$css = $block.find('style');
-		if ($js.length != 0) {
-			$block.append('<p>javascript:</p>')
-				.append($('<pre>').addClass("brush: js").html($js.html()));
-		}
-		if ($css.length != 0) {
-			$block.append('<p>CSS:</p>')
-				.append($('<pre>').addClass("brush: css").html($css.html()));
+		if ($js.length != 0 || $css.length != 0) {
+			$blockSrc = $('<div>').addClass("demo-block-src");
+			if ($js.length != 0) {
+				$blockSrc.append('<p>javascript:</p>')
+					.append($('<pre>').addClass("brush: js").html($js.html()));
+			}
+			if ($css.length != 0) {
+				$blockSrc.append('<p>CSS:</p>')
+					.append($('<pre>').addClass("brush: css").html($css.html()));
+			}
+			$block.append($blockSrc);
 		}
 	});
 	SyntaxHighlighter.all();
@@ -41,7 +70,7 @@ function initDemoPage($) {
 			$('div#main').append($('body').children());
 			$('body').append($('div#demo-container'));
 			$('#demo-menu').appendTo('div#sidebar').menu();
-			$('.info').appendTo('div#sidebar');
+			$('.demo-info').appendTo('div#sidebar');
 		}
 	});
 }
