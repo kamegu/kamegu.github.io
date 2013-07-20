4d5b0ea3b0dfbc7b480f69eeb87b59567872bfb2
kamegu <kamegu@server.fake>
2013-07-20 14:29:10 +0900

    create demo index. add google analytics

--- js/demo.js
+++ js/demo.js
@@ -91,3 +91,12 @@ function _initDemoPage($) {
 		}
 	});
 }
+
+/*Google Analytics*/
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-40142319-2', 'kamegu.github.io');
+  ga('send', 'pageview');
